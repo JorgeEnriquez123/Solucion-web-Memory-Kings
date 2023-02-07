f84e7455e700854c3c276582fc6e2ca486343336
Jorge <joseph_adagio_7700@hotmail.com>
2023-02-06 22:45:14 -0500

    Primera subida

---
+++ src/main/java/com/example/memorykings/MemorykingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.memorykings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorykingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorykingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memorykings/MemorykingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memorykings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorykingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
